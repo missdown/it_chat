c475119a366e5a32b9eb5999cfc92d795bd0d71a
missdown <a1998317@126.com>
2017-08-08 22:26:38 +0800

    Add files via upload

--- servers_links.py
+++ servers_links.py
@@ -6,6 +6,9 @@ import time
 from tornado import gen
 import logging
 import pymongo
+from subprocess import call
+import os
+import face
 
 itchatmp.set_logging(showOnCmd=True, loggingFile=None, loggingLevel=logging.INFO)
 itchatmp.update_config(itchatmp.WechatConfig(
@@ -17,7 +20,7 @@ itchatmp.update_config(itchatmp.WechatConfig(
 
 app = Flask(__name__)
 app.debug = True
-
+count = 0
 
 @itchatmp.access_token
 def get_access_token(accessToken=None):
@@ -51,17 +54,35 @@ def it_index():
         if msgType == "text":
             content = xml.find('Content').text
             if content == "弹幕发送":
-                return reply(fromUser, toUser, "弹幕姬发动中")
+                return reply_text(fromUser, toUser, "弹幕姬发动中")
+            return reply_text(fromUser, toUser, reply(1, content))
         elif msgType == "image":
             image_url = xml.find('MediaId').text
-            print(image_url)
+            num = save_image(image_url)
+            face.detect(num)
+            return reply_image(fromUser, toUser, num)
         else:
             return reply_text(fromUser, toUser, "我只懂文字")
 
 
 @app.route("/ok")
 def ok():
-    return "i'am ok"
+    return "The server is working"
+
+
+def save_image(image_url):
+    if [True for i in os.listdir('./') if i == 'image']:
+        pass
+    else:
+        os.mkdir("./image")
+    r = itchatmp.messages.download(image_url)
+    if 'File' in r:
+        with open(r.get('./image', '{}.jpg'.format(count)), 'wb') as f:
+            f.write(r['File'].getvalue())
+    else:
+        print(r)
+    count += 1
+    return count - 1
 
 
 def reply_text(to_user, from_user, content):
@@ -84,17 +105,40 @@ def reply_text(to_user, from_user, content):
                int(time.time() * 1000), content)
 
 
+def reply_image(to_user, from_user, image_id):
+    return """
+    <xml>
+        <ToUserName><![CDATA[{}]]></ToUserName>
+        <FromUserName><![CDATA[{}]]></FromUserName>
+        <CreateTime>{}</CreateTime>
+        <MsgType><![CDATA[image]]></MsgType>
+        <Image>
+        <MediaId><![CDATA[{}]]></MediaId>
+        </Image>
+    </xml>
+    """.format(to_user, from_user, int(time.time() * 1000), upload(image_id))
+
+
+def upload_image(image_id):
+    r = itchatmp.messages.upload(itchatmp.content.IMAGE, './image/{}.jpg'.format(count))
+    if r:
+        print(r['media_id'])
+        return r['media_id']
+    else:
+        print('Failed: \n%s' % r)
+
+
 def reply(openid, msg):
     #简单地翻转一下字符串就回复用户
     return msg[::-1]
 
 
 if __name__ == '__main__':
-    r = itchatmp.messages.upload(itchatmp.content.IMAGE, 'image.jpg')
-    if r:
-        print(r['media_id'])
-    else:
-        print('Failed: \n%s' % r)
+    # r = itchatmp.messages.upload(itchatmp.content.IMAGE, 'image.jpg')
+    # if r:
+    #     print(r['media_id'])
+    # else:
+    #     print('Failed: \n%s' % r)
     r = get_access_token()
     print(r)
     app.run(host="0.0.0.0", port=80)
